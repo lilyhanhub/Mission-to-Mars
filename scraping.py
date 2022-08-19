a5b62beb78349b8e131589df67ac98e98344a64d
lilyhanhub <106350763+lilyhanhub@users.noreply.github.com>
2022-08-18 22:26:23 -0700

    Integrate MongoDB into the web app

--- scraping.py
+++ scraping.py
@@ -24,32 +24,27 @@ def scrape_all():
    # Stop webdriver and return data
     browser.quit()
     return data
-   
+
 def mars_news(browser):
-    # Scrape Mars News
-    # Visit the mars nasa news site
-    url = 'https://data-class-mars.s3.amazonaws.com/Mars/index.html'
-    browser.visit(url)
-
-    # Optional delay for loading the page
-    browser.is_element_present_by_css('div.list_text', wait_time=1)
-
-    # Convert the browser html to a soup object and the quit the browser
-    html = browser.html
-    news_soup = soup(html, 'html.parser')
-    
-    # Add try/except for error handling
-    try:
-        slide_elem = news_soup.select_one('div.list_text')
-        # Use the parent element to find the first 'a' tag and save it as 'news_title'
-        news_title = slide_elem.find('div', class_='content_title').get_text()
-        # Use the parent element to find the paragraph text
-        news_p = slide_elem.find('div', class_='article_teaser_body').get_text()
-
-    except AttributeError:
-        return None, None
-
-    return news_title, news_p
+   # Scrape Mars News
+   # Visit the mars nasa news site
+   url = 'https://redplanetscience.com/'
+   browser.visit(url)
+   # Optional delay for loading the page
+   browser.is_element_present_by_css('div.list_text', wait_time=1)
+   # Convert the browser html to a soup object and then quit the browser
+   html = browser.html
+   news_soup = soup(html, 'html.parser')
+   # Add try/except for error handling
+   try:
+       slide_elem = news_soup.select_one('div.list_text')
+       # Use the parent element to find the first 'a' tag and save it as 'news_title'
+       news_title = slide_elem.find("div", class_="content_title").get_text()
+       # Use the parent element to find the paragraph text
+       news_p = slide_elem.find("div", class_="article_teaser_body").get_text()
+   except AttributeError:
+       return None, None
+   return news_title, news_p
 
 # ### JPL Space Images Featured Image
 
